879bb0706f0e5056644bc746fc17d15a67dd5d4b
LeRoi Vladimir <banyamkimbi@vrfound.org>
2023-06-04 20:06:12 +0100

    created user model

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-
+mongoose.connect(
+    "mongodb+srv://mongodb_tut:mongodb_tut@test.wecfu.mongodb.net/recipeleroi?retryWrites=true&w=majority"
+)
 
 app.listen(3000, () => console.log("SERVER STARTED!"));
